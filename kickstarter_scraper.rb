c35100610370028026642d39132f429a56f46d07
virtual-frame-7789 <mttwright@gmail.com>
2019-10-19 18:28:14 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,11 @@
-require nokogiri
+require "nokogiri"
+require "pry"
 # require libraries/modules here
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+
+create_project_hash
